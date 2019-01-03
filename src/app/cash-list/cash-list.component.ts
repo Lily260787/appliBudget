5868b30ffb2339de4a525c75e8c1188850bbaaf4
julie <julie.da-prato@live.fr>
2019-01-03 18:27:25 +0100

    adding new object OK

--- src/app/cash-list/cash-list.component.ts
+++ src/app/cash-list/cash-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import { CashService } from '../services/cash.service';
 import { Cash } from '../models/Cash.model';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-cash-list',
@@ -8,17 +9,40 @@ import { Cash } from '../models/Cash.model';
   styleUrls: ['./cash-list.component.css']
 })
 export class CashListComponent implements OnInit {
-
+  cashForm: FormGroup;
+  // Form state
+  loading = false;
+  success = false;
   constructor(
-
+    private cashService: CashService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
+    this.cashForm = this.fb.group(
+      {
+        amount: ['', [Validators.required]],
+        status : ['', [Validators.required]],
+        label : ['', [Validators.required]],
+        comment : [],
+        date : ['', [Validators.required]]
+      }
+    );
+  }
+  async submitForm() {
+    this.loading = true;
 
+    try {
+      await this.create(this.cashForm.value);
+      this.success = true;
+    } catch (err) {
+      console.error(err);
+    }
+    this.loading = false;
   }
 
-  /*create(cash: Cash) {
-    this.cashService.create(cash);
+  create() {
+    this.cashService.createCash(this.cashForm.value);
   }
 
   update(cash: Cash) {
@@ -27,6 +51,6 @@ export class CashListComponent implements OnInit {
 
   delete(id: string) {
     this.firestore.doc('cash/' + id).delete();
-  }*/
+  }
 
 }
